cdb9e5b53952c7b31e56cf123aa63a2f1bd111db
Thomas Davidson <87674288+Thomas25504@users.noreply.github.com>
2026-02-24 22:59:06 +0000

    Changed Title and added screenshot for ReadMe

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using OpenTKTest;
 
-using (Game game = new Game(1260, 720, "OpenTK Test"))
+using (Game game = new Game(1260, 720, "OpenTK Voxel Engine - Minecraft Clone"))
 {
     game.Run();
 }
--- obj/Debug/net10.0/openTKTest.AssemblyInfo.cs
+++ obj/Debug/net10.0/openTKTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("openTKTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06999618230c75c975d52cd6d8f7aaa80b43c7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f3446948b75a665cb66b96a80186201df8551c")]
 [assembly: System.Reflection.AssemblyProductAttribute("openTKTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("openTKTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
